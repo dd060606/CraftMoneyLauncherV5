00001f26a53724abe537aa05528404b7707af079
dd060606 <dd06arceus@gmail.com>
2020-08-17 11:43:04 +0200

    Patch of bugs

--- src/main/java/fr/dd06/craftmoney/launcher/CraftMoneyGame.java
+++ src/main/java/fr/dd06/craftmoney/launcher/CraftMoneyGame.java
@@ -5,18 +5,21 @@ import fr.dd06.apis.mclauncher.minecraft.GameTweak;
 import fr.dd06.apis.mclauncher.minecraft.GameType;
 import fr.dd06.apis.mclauncher.minecraft.GameVersion;
 import fr.dd06.craftmoney.CraftMoneyLauncher;
+import fr.flowarg.flowlogger.Logger;
 import fr.flowarg.flowupdater.FlowUpdater;
 import fr.flowarg.flowupdater.utils.BuilderArgumentException;
 import fr.flowarg.flowupdater.versions.IForgeVersion;
 import fr.flowarg.flowupdater.versions.IVanillaVersion;
+import fr.flowarg.flowupdater.versions.NewForgeVersion;
 import fr.flowarg.flowupdater.versions.VersionType;
 import fr.flowarg.flowupdater.versions.download.IProgressCallback;
+import fr.flowarg.flowupdater.versions.download.Mod;
 import fr.flowarg.flowupdater.versions.download.Step;
 
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.util.List;
 
 
 public class CraftMoneyGame {
@@ -29,14 +32,25 @@ public class CraftMoneyGame {
 
     public static File CRAFTMONEY_GAME_DIR = GAME_INFOS.getGameDir();
     public static File CRAFTMONEY_LOG_DIR = new File(CRAFTMONEY_GAME_DIR, "logs/");
+    private final IVanillaVersion.Builder versionBuilder = new IVanillaVersion.Builder("1.12.2");
+    private final IVanillaVersion version = versionBuilder.build(false, VersionType.FORGE);
+
+    private FlowUpdater updater;
+
+    private void update(File dir, IProgressCallback callback) throws IOException {
+
+        List<Mod> mods = Mod.getModsFromJson("");
+        Logger logger = new Logger("[CraftMoney]", new File(CRAFTMONEY_LOG_DIR, "last-launcher.log"));
+        try {
+            updater = new FlowUpdater.FlowUpdaterBuilder().withVersion(version).withLogger(logger).withSilentUpdate(true).withProgressCallback(callback).build();
+        } catch (BuilderArgumentException e) {
+            e.printStackTrace();
+        }
 
 
-    private void update(File dir, IProgressCallback callback)throws IOException
-    {
-        final IVanillaVersion.Builder versionBuilder = new IVanillaVersion.Builder("1.12.2");
-        final IVanillaVersion version = versionBuilder.build(false, VersionType.VANILLA);
-        final FlowUpdater updater = new FlowUpdater.FlowUpdaterBuilder().withVersion(version).withLogger(new Logger()).withSilentUpdate(true).withProgressCallback(callback).build();
-        updater.update(dir, false);
+        updater.setForgeVersion(new NewForgeVersion("1.12.2-14.23.5.2854", version, logger, callback, mods, false));
+
+        updater.update(CRAFTMONEY_GAME_DIR , false);
     }
 
     public void start() {
